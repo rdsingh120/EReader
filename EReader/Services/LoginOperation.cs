2e4e424941f4e15cbda1309d4abfbc3d6f049b95
Ripudaman Singh <rdsingh120@gmail.com>
2025-10-02 00:29:02 +0530

    add login operations

---
+++ EReader/Services/LoginOperation.cs
@@ -0,0 +1,16 @@
+﻿using EReader.Models;
+
+namespace EReader.Services
+{
+    static class LoginOperation
+    {
+
+        public static async Task<bool> CheckCredentails(string username, string password)
+        {
+            var user = await new DynamoDBOperations().GetUser(username);
+            if (user == null) return false;
+            if(user.Password != password) return false;
+            return true;
+        }
+    }
+}
