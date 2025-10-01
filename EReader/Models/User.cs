3f4e27f3ea74e9d2b86da980c9f265ad08115860
Ripudaman Singh <rdsingh120@gmail.com>
2025-10-01 23:43:15 +0530

    update user class

--- EReader/Models/User.cs
+++ EReader/Models/User.cs
@@ -2,7 +2,7 @@
 {
     class User
     {
-        public String Email { get; set; }
+        public string UserName { get; set; }
         public string Password { get; set; }
         public List<Book> Books { get; set; }
     }
