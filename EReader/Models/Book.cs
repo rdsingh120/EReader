8257251d0db11e173409b23c64ea0b99b4d326c1
Ripudaman Singh <rdsingh120@gmail.com>
2025-10-01 23:04:22 +0530

    add book class

---
+++ EReader/Models/Book.cs
@@ -0,0 +1,10 @@
+﻿namespace EReader.Models
+{
+    public class Book
+    {
+        public string Name { get; set; }
+        public int LastReadPageNumber { get; set; }
+        public string LastReadTime { get; set; }
+
+    }
+}
