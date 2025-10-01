ff1790ecbacedac65ea3ab3fc7e39ddb4c96024f
Ripudaman Singh <rdsingh120@gmail.com>
2025-10-01 23:04:08 +0530

    add user class

---
+++ EReader/Models/User.cs
@@ -0,0 +1,9 @@
+﻿namespace EReader.Models
+{
+    class User
+    {
+        public String Email { get; set; }
+        public string Password { get; set; }
+        public List<Book> Books { get; set; }
+    }
+}
