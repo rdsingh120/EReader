d9a91926bc8f9f63b7b0f191993c596326695749
Ripudaman Singh <rdsingh120@gmail.com>
2025-10-01 22:04:09 +0530

    Add project files.

---
+++ EReader/MainWindow.xaml.cs
@@ -0,0 +1,19 @@
+﻿using EReader.Views;
+using System.Windows;
+
+namespace EReader
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void LoginButton_Click(object sender, RoutedEventArgs e)
+        {
+            EBookReader eBookReader = new EBookReader();
+            eBookReader.ShowDialog();
+        }
+    }
+}
