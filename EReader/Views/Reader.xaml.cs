a1c6341095d1202a5124e6ba4ee3a28a3ef0848c
Ripudaman Singh <rdsingh120@gmail.com>
2025-10-02 19:34:40 +0530

    add reader window with LoadPdf method

---
+++ EReader/Views/Reader.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.IO;
+using System.Windows;
+
+namespace EReader.Views
+{
+    public partial class Reader : Window
+    {
+        public Reader()
+        {
+            InitializeComponent();
+        }
+
+        public void LoadPdf(Stream stream, int pageNumber)
+        {
+            pdfViewer.Load(stream);
+            pdfViewer.GotoPage(pageNumber);
+            pdfViewer.Visibility = Visibility.Visible;
+        }
+    }
+}
