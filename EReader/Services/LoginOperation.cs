edeafd6660446a6a45e365487548824f81595c2c
Ripudaman Singh <rdsingh120@gmail.com>
2025-10-02 08:38:42 +0530

    update CheckCredentails method

--- EReader/Services/LoginOperation.cs
+++ EReader/Services/LoginOperation.cs
@@ -5,12 +5,12 @@ namespace EReader.Services
     static class LoginOperation
     {
 
-        public static async Task<bool> CheckCredentails(string username, string password)
+        public static async Task<User?> CheckCredentails(string username, string password)
         {
             var user = await new DynamoDBOperations().GetUser(username);
-            if (user == null) return false;
-            if(user.Password != password) return false;
-            return true;
+            if (user == null) return null;
+            if(user.Password != password) return null;
+            return user;
         }
     }
 }
