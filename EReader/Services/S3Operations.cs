bb05838ce45b55c3cbf0986f109c3d77d0e9d612
Ripudaman Singh <rdsingh120@gmail.com>
2025-10-02 19:40:23 +0530

    add s3operations to get pdf stream

---
+++ EReader/Services/S3Operations.cs
@@ -0,0 +1,33 @@
+﻿using Amazon.Runtime;
+using Amazon.S3;
+using System.IO;
+
+namespace EReader.Services
+{
+    internal class S3Operations
+    {
+        private AWSCredentials credentials;
+        private AmazonS3Client client;
+
+        private readonly string? accessKey = Environment.GetEnvironmentVariable("AWS_ACCESS_KEY_ID");
+        private readonly string? secretKey = Environment.GetEnvironmentVariable("AWS_SECRET_ACCESS_KEY");
+
+        private readonly string bucketName = "mybucketforereaderbooks";
+        private readonly string folder = "books/";
+        private readonly string extension = ".pdf";
+
+        public S3Operations()
+        {
+            credentials = new BasicAWSCredentials(accessKey, secretKey);
+            client = new AmazonS3Client(credentials, Amazon.RegionEndpoint.USEast1);
+        }
+        public async Task<Stream> GetBook(string bookName)
+        {
+            var response = await client.GetObjectAsync(bucketName, folder + bookName + extension);
+            MemoryStream memoryStream = new MemoryStream();
+            await response.ResponseStream.CopyToAsync(memoryStream);
+            memoryStream.Position = 0;
+            return memoryStream;
+        }
+    }
+}
